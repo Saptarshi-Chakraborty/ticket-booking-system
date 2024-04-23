63a20d7a6100a12561d39e273704cbe175a6aa91
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2024-04-23 10:36:32 +0530

    Update CONSTANTS.js

--- frontend/CONSTANTS.js
+++ frontend/CONSTANTS.js
@@ -1,10 +1,18 @@
+import getEnvironment from "./src/utils/environment.js"
+
 const CONSTANTS = {
     "JWT_SECRET": "wfr43Ge5$2w*d9",
     "ALL_TICKETS_KEY": "allTickets",
     "AUTH_TOKEN_KEY": "AUTH_TOKEN",
     "addNewStationModalId": "addNewStationModal",
     "API": {
-        "BASE_URL": `${window.location.protocol}//${window.location.hostname}:3000`,
+        "REMOTE_BASE_URL": "https://ticket-booking-system-4hmm.onrender.com/",
+        "LOCAL_BASE_URL": `${window.location.protocol}//${window.location.hostname}:3000`,
+
+        "BASE_URL": `${getEnvironment() === "development" ?
+            `${window.location.protocol}//${window.location.hostname}:3000` :
+            "https://ticket-booking-system-4hmm.onrender.com"}`,
+
         "addNewStation": "/add-station",
         "getAllStations": "/stations",
         "createRegularTicket": "/regular-ticket",
